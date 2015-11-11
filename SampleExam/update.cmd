c0e8df48e18026718df8a767f38c63b01204e7ae
Mridsole <mridsole@gmail.com>
2015-11-11 23:18:02 +1100

    updated exam and solutions

--- SampleExam/update.cmd
+++ SampleExam/update.cmd
@@ -2,7 +2,6 @@
 cd /d E:/python/elec2104/SampleExam
 jupyter nbconvert --to html SampleExam.ipynb
 jupyter nbconvert --to html SampleExamSOlutions.ipynb
-@echo on
 cd ../
 git add SampleExam/*
 git commit -m "updated exam and solutions"
